ac2b2483883b27512ba67abc67b00e95d0f58e25
Joao Victor <vektor5665@hotmail.com>
2025-05-23 14:28:29 -0300

    att

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   create(data: CreateUserDto) {
-    return this.prisma.user.create({
-      data: {
-        name: data.name,
-        email: data.email,
-        password: data.password, // <- Campo correto
-      },
-    });
-  }
+  const userCreateArgs: Prisma.UserCreateArgs = {
+    data: {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    },
+  };
+
+  return this.prisma.user.create(userCreateArgs);
+}
 
   findAll() {
     return this.prisma.user.findMany();
