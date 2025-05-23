fa326a407639a7ee240482bea505f148d064c78f
Joao Victor <vektor5665@hotmail.com>
2025-05-23 13:13:40 -0300

    atualziaçao 1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000
+  app.enableCors({
+    origin: '*', // ou especifique: ['http://127.0.0.1:5500']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
    app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
+   
 }
 bootstrap();
